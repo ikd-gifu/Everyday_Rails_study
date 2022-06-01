952c2feadb2631fdf9f11c38eb6fb976ff011533
EC2 Default User <ec2-user@ip-172-31-0-184.ap-northeast-1.compute.internal>
2022-06-01 13:24:44 +0000

    20220601 Finish-my-03-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # validates :first_name, presence: true
+  validates :first_name, presence: true
   validates :last_name, presence: true
 
   has_many :projects, dependent: :destroy
