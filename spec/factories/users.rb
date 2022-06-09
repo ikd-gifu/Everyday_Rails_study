7c6da63fb9862e2918647d28b0599e6f95e981aa
EC2 Default User <ec2-user@ip-172-31-0-184.ap-northeast-1.compute.internal>
2022-06-09 06:17:17 +0000

    20200609 Add-sequence

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { "Aaron" }
     last_name { "Summer" }
-    email { "tester@example.com" }
+    seqence(:email) { |n| "tester#{n}@example.com" }
     password { "dottle-nouveau-pavilion-tighit-furze" }
   end
 end
