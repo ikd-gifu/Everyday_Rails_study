88800ff7b9169ef2dc87513cc667ce1f5a41d099
EC2 Default User <ec2-user@ip-172-31-0-184.ap-northeast-1.compute.internal>
2022-06-18 05:45:44 +0000

    20220618 Add-projects-controller-POST-test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,74 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
-  describe "#index" do
-    # 認証済みのユーザーとして
-    context "as an authenticated user" do
-      before do
-        @user = FactoryBot.create(:user)
-      end
-
-    # 正常にレスポンスを返すこと
-    it "responds successfully" do
-      sign_in @user
-      get :index
-      expect(response).to be_successful
-    end
-    
-    # 200レスポンスを返すこと
-    it "returns a 200 response" do
-      sign_in @user
-      get :index
-      
-    end
-  end
-  
-  # ゲストとして
-    context "as a guest" do
-      # 302レスポンスを返すこと
-      it "returns a 302 response" do
-        get :index
-        expect(response).to have_http_status "302"
-      end
-      
-      # サインイン画面にリダイレクトすること
-      it "redirects to the sign_in page" do
-        get :index
-        expect(response).to redirect_to "/users/sign_in"
-      end
-    end
-  end
-  
-  describe "#show" do
-    # 認可されたユーザーとして
-    context "as an authorized user" do
-      before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-      end
-      
-      # 正常にレスポンスを返すこと
-      it "responds successfully" do
-        sign_in @user
-        get :show, params: { id: @project.id }
-        expect(response).to be_successful
-      end
-    end
-    
-    # 認可されていないユーザーとして
-    context "as an unauthorized user" do
-      before do
-        @user = FactoryBot.create(:user)
-        other_user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: other_user)
-      end
-      
-      # ダッシュボードにリダイレクトすること
-      it "redirects to the dashboard" do
-        sign_in @user
-        get :show, params: { id: @project.id }
-        expect(response).to redirect_to root_path
-      end
-    end
-  end
+  describe "# create" do
+   # 認証済みのユーザーとして context "as an authenticated user" do
+   before do
+    @user = FactoryBot.create(:user)
+   end
+   
+   # プロジェクトを追加できること
+   it "adds a project" do
+     project_params = FactoryBot.attributes_for(:project)
+       sign_in @user
+       expect {
+        post :create, params: { project: project_params }
+       }.to change(@user.projects, :count). by(1)
+     end
+   end
+   
+   # ゲストとして
+   context "as a guest" do
+     # 302 レスポンスを返すこと
+     it "returns a 302 response" do
+       project_params = FactoryBot.attributes_for(:project)
+       post :create, params: { project: project_params }
+       expect(response).to have_http_status "302"
+     end
+   
+     # サインイン画面にリダイレクトすること
+     it "redirects to the sign-in page" do
+       project_params = FactoryBot.attributes_for(:project)
+       post :create, params: { project:project_params }
+       expect( response). to redirect_to "/users/sign_in"
+     end
+   end
 end
+
