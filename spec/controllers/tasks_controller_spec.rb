71f241b76c22e8314bc895bca9f2adb34c45abb3
EC2 Default User <ec2-user@ip-172-31-0-184.ap-northeast-1.compute.internal>
2022-07-11 10:36:31 +0000

    20220711 Add-tasks_controller-create-json-test

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -4,18 +4,39 @@ RSpec.describe TasksController, type: :controller do
   before do
     @user = FactoryBot.create(:user)
     @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name:"Test task")
+    @task = @project.tasks.create!(name: "Test task")
   end
   
-  describe "# show" do
+  # show のテストは省略 ...
+  
+  describe "# create" do
     # JSON 形式でレスポンスを返すこと
     it "responds with JSON formatted output" do
-      sign_in @user
-      get :show, format: :json,
-        params: { project_id: @project.id, id: @task.id }
-      expect(response.content_type).to include "application/json"
-    end
+    new_task = { name: "New test task" }
+    sign_in @user
+    post :create, format: :json,
+      params: { project_id: @project.id, task: new_task }
+    expect( response.content_type).to include "application/json"
   end
   
-  # 他のテストは省略 ...
+  # 新しいタスクをプロジェクトに追加すること
+  it "adds a new task to the project" do
+    new_task = { name: "New test task" }
+    sign_in @user
+    expect {
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+    }.to change(@project.tasks, :count). by(1)
+  end
+  
+  # 認証を要求すること
+  it "requires authentication" do
+    new_task = { name: "New test task" }
+    # ここではあえてログインしない ...
+    expect {post :create, format: :json,
+    params: { project_id: @project.id, task: new_task }
+    }.to_not change(@project.tasks, :count)
+    expect(response).to_not be_successful
+    end
+  end
 end
